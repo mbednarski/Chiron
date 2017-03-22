09147d2dfe50189595be2a2cd077f20fa2d46295
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-03-22 16:40:55 +0100

    Q agent

---
+++ q_agent.py
@@ -0,0 +1,161 @@
+from __future__ import print_function, division
+
+import copy
+import abc
+import itertools
+
+import numpy as np
+
+import gym
+
+
+class Featurizer(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def transform(self, observation):
+        """Transform an observation into features"""
+
+
+class NullFeaturizer(Featurizer):
+    def __init__(self, env):
+        self.observation_space_shape = env.observation_space.n
+        self.features_shape = self.observation_space_shape
+
+    def transform(self, observation):
+        return observation
+
+
+class Policy(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def select_action(self, state):
+        pass
+
+
+class GreedyPolicy(Policy):
+    def __init__(self, action_scorer):
+        self.action_scorer = action_scorer
+
+    def select_action(self, state):
+        values = self.action_scorer(state)
+        candidates = np.argwhere(values == np.amax(values))
+        return np.random.choice(candidates.flatten())
+
+
+class EpsilonGreedyPolicy(Policy):
+    def __init__(self, action_scorer):
+        self.action_scorer = action_scorer
+        self.epsilon = 1.0
+        self.decay = 0.99
+
+    def select_action(self, state):
+        self.epsilon *= 0.99
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(low=0, high=4)
+
+        values = self.action_scorer(state)
+        candidates = np.argwhere(values == np.amax(values))
+        return np.random.choice(candidates.flatten())
+
+
+class QAgent(object):
+    def __init__(self, env):
+        self.env = env
+        self.params = dict()
+        self.n_actions = env.action_space.n
+        self.n_states = env.observation_space.n
+
+        self.alpha = 0.0
+        self.gamma = 0.0
+        self.initial_Q = 0.0
+        self.epsilon = 0.0
+        self.max_episodes = 100000
+        self.max_steps = 10000
+        self.wins = 0
+
+        self.featurizer = NullFeaturizer(self.env)
+        self.Q = np.ones((self.n_actions, self.featurizer.features_shape)) * self.initial_Q
+
+        self.set_parameters(self.get_default_parameters())
+
+        self.policy = EpsilonGreedyPolicy(lambda s: self.Q[:, s])
+
+    def get_default_parameters(self):
+        return {
+            'alpha': 0.3,
+            'gamma': 0.99,
+            'initial_Q': 0.0,
+            'epsilon': 1.0
+        }
+
+    def set_parameters(self, params):
+        self.params.update(params)
+        self.alpha = self.params['alpha']
+        self.gamma = self.params['gamma']
+        self.initial_Q = self.params['initial_Q']
+        self.epsilon = self.params['epsilon']
+
+    def get_parameters(self):
+        return copy.deepcopy(self.params)
+
+    def episode(self, i_episode):
+        creward = 0
+        obs = self.env.reset()
+        state = self.featurizer.transform(obs)
+
+        for t in itertools.count():
+            if t == self.max_steps:
+                print('aborting...')
+                return
+
+            if i_episode == self.max_episodes - 1:
+                self.env.render()
+
+            action = self.policy.select_action(state)
+
+            new_obs, reward, done, _ = self.env.step(action)
+
+            new_state = self.featurizer.transform(new_obs)
+
+            self.learn(state, new_state, action, reward, done)
+
+            state = new_state
+            creward += reward
+
+            if done:
+                print('Episode {} finished with score {} after {} steps'.format(i_episode, creward, t))
+                if reward == 1:
+                    self.wins += 1
+                return
+
+    def learn(self, state, new_state, action, reward, done):
+        if done:
+            delta = reward - self.Q[action, state]
+        else:
+            delta = reward + self.gamma * np.max(self.Q[:, new_state]) - self.Q[action, state]
+
+        self.Q[action, state] += self.alpha * delta
+
+    def game(self):
+        for i_episode in range(self.max_episodes):
+            self.episode(i_episode)
+
+if __name__ == '__main__':
+    env = gym.make('FrozenLake-v0')
+    agent = QAgent(env)
+    agent.game()
+    print(agent.Q)
+    U = agent.Q.mean(axis=0).reshape((4, 4))
+    for row in range(4):
+        print(U[row])
+    print(agent.wins)
+
+    q = agent.Q
+    for y in range(4):
+        for x in range(4):
+            a = np.argmax(q[:, y*4 +x])
+            print(a, end='')
+        print()
+
