520d4065cb097af3c010ab0453ba32ea26d47315
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-03-24 14:17:12 +0100

    sarsa

--- policies.py
+++ policies.py
@@ -12,6 +12,30 @@ class Policy(object):
     def select_action(self, state):
         pass
 
+    def episode_end(self):
+        pass
+
+
+class BoltzmannPolicy(Policy):
+    def __init__(self, action_scorer, temperature=10, decay=0.999):
+        self.action_scorer = action_scorer
+        self.decay = decay
+        self.temperature = temperature
+
+    def select_action(self, state):
+        probs = np.divide(
+            np.exp(np.divide(self.action_scorer(state), self.temperature)),
+            np.sum(
+                np.exp(np.divide(self.action_scorer(state), self.temperature))
+            )
+        )
+        return np.random.choice(range(probs.shape[0]), p=probs)
+
+    def episode_end(self):
+        self.temperature *= self.decay
+        if self.temperature < 0.01:
+            self.temperature = 0.01
+
 
 class GreedyPolicy(Policy):
     def __init__(self, action_scorer):
@@ -21,3 +45,21 @@ class GreedyPolicy(Policy):
         values = self.action_scorer(state)
         candidates = np.argwhere(values == np.amax(values))
         return np.random.choice(candidates.flatten())
+
+
+class EpsilonGreedyPolicy(Policy):
+    def __init__(self, action_scorer, decay=0.99):
+        self.decay = decay
+        self.action_scorer = action_scorer
+        self.epsilon = 1.0
+
+    def select_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(low=0, high=4)
+
+        values = self.action_scorer(state)
+        candidates = np.argwhere(values == np.amax(values))
+        return np.random.choice(candidates.flatten())
+
+    def episode_end(self):
+        self.epsilon *= self.decay
--- sarsa_agent.py
+++ sarsa_agent.py
@@ -5,17 +5,18 @@ import abc
 import itertools
 
 import numpy as np
+# np.seterr(all='raise')
 import seaborn as sns
 
 import gym
 from gym import wrappers
 
 from featurizers import NullFeaturizer
-from policies import GreedyPolicy, BoltzmannPolicy
+from policies import GreedyPolicy, EpsilonGreedyPolicy, BoltzmannPolicy
 
 
-class QAgent(object):
-    def __init__(self, env, max_episodes=1000, max_steps=10000):
+class SarsaAgent(object):
+    def __init__(self, env, max_episodes=10000, max_steps=10000):
         self.env = env
         self.params = dict()
         self.n_actions = env.action_space.n
@@ -25,15 +26,17 @@ class QAgent(object):
         self.max_steps = max_steps
         self.history = np.zeros(self.max_episodes)
 
+        self.learning = True
+
         self.set_parameters(self.get_default_parameters())
 
     # noinspection PyAttributeOutsideInit
     def construct(self):
         self.featurizer = NullFeaturizer(self.env)
         self.Q = np.ones((self.n_actions, self.featurizer.shape)) * self.initial_Q
-        # self.policy = GreedyPolicy(lambda s: self.Q[:, s])
+        self.policy = GreedyPolicy(lambda s: self.Q[:, s])
+        # self.policy = EpsilonGreedyPolicy(lambda s: self.Q[:, s])
         self.policy = BoltzmannPolicy(lambda s: self.Q[:, s])
-
 
     def get_default_parameters(self):
         return {
@@ -59,51 +62,61 @@ class QAgent(object):
         obs = self.env.reset()
         state = self.featurizer.transform(obs)
 
+        action = self.policy.select_action(state)
+
         for t in itertools.count():
             if t == self.max_steps:
                 print('aborting...')
                 return
 
-            action = self.policy.select_action(state)
+            # action = self.policy.select_action(state)
 
             new_obs, reward, done, _ = self.env.step(action)
-
             new_state = self.featurizer.transform(new_obs)
 
-            self.learn(state, new_state, action, reward, done)
+            new_action = self.policy.select_action(new_state)
+
+            # if self.learning:
+            self.learn(state, new_state, action, new_action, reward, done)
 
             state = new_state
+            action = new_action
             episode_reward += reward
 
             if done:
+                # if episode_reward == 15.0:
+                #     self.learning = False
                 self.history[i_episode] = episode_reward
                 # print('Episode {} finished with score {} after {} steps'.format(i_episode, reward, t))
                 return
 
-    def learn(self, state, new_state, action, reward, done):
+    def learn(self, state, new_state, action, new_action, reward, done):
         if done:
             delta = reward - self.Q[action, state]
         else:
-            delta = reward + self.gamma * np.max(self.Q[:, new_state]) - self.Q[action, state]
+            delta = reward + self.gamma * self.Q[new_action, new_state] - self.Q[action, state]
 
         self.Q[action, state] += self.alpha * delta
 
     def game(self):
         for i_episode in range(self.max_episodes):
             self.episode(i_episode)
+            self.policy.episode_end()
+            # self.alpha *= 0.993
 
 
 if __name__ == '__main__':
-    env = gym.make('Taxi-v2')
+    env = gym.make('FrozenLake-v0')
     env = wrappers.Monitor(env, '/tmp/fl/2', force=True)
-    agent = QAgent(env, max_episodes=10000)
+    agent = SarsaAgent(env, max_episodes=10000)
     agent.set_parameters({
-            'alpha': 0.2,
+            'alpha': 0.4,
             'gamma': 0.99,
             'initial_Q': 0.0
     })
     agent.game()
     env.close()
+
     means = np.zeros(agent.max_episodes)
     means2 = np.zeros(agent.max_episodes)
     for e in range(agent.max_episodes):
@@ -124,3 +137,5 @@ if __name__ == '__main__':
     sns.tsplot(means, color='r')
     sns.tsplot(means2, color='g')
     sns.plt.show()
+
+
