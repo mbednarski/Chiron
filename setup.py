085d976b47c59dffab0ffb4472c70adc56d36e10
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-04-05 19:41:44 +0200

    CI stub

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='chiron',
+    version='0.0.1',
+    packages=['agents', 'test_agents'],
+    url='',
+    license='',
+    author='Mateusz Bednarski',
+    author_email='',
+    description=''
+)
