d828d608ccb420c03ffcf5552e552e9157f37082
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-04-07 18:33:17 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='chiron',
+    version='',
+    packages=['chiron.tests', 'chiron.agents'],
+    url='',
+    license='',
+    author='Mateusz Bednarski',
+    author_email='',
+    description='', requires=['gym']
+)
