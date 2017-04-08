b443ef997300b356dde2542ca87fa73b6edfc887
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-04-08 14:29:14 +0200

    Project layout

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name='chiron',
     version='',
-    packages=['chiron', 'chiron.tests', 'chiron.agents'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     url='',
     license='',
     author='Mateusz Bednarski',
     author_email='',
-    description='', requires=['gym']
+    description=''
 )
